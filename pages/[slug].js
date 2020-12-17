5a28c7d58e25f224200631f801a5bd28e36fe4fb
fabianojp06 <64838654+fabianojp06@users.noreply.github.com>
2020-12-17 06:39:58 -0300

    Add files via upload

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,40 +1,38 @@
-
 import React, { useEffect } from "react";
-import Prismic from 'prismic-javascript';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
+import Prismic from "prismic-javascript";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 const RedirectTo = () => {
-    const router = useRouter();
-    useEffect(() =>{
-        setTimeout (() =>{
-         router.push("/")
-        }, 2000);
-    },[]);
-    return(
-        <div className="w-1/2 mx-auto text-center py-4">
-            <head>
-                <title>Página não encontrada</title>
-            </head>
-            <h1 className="font-bold text-4xl ">Destino não encontrado.</h1>
-            <p>Você será redirecionado para a Central de Links...</p>
-            </div>
-        );   
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 2000);
+  }, []);
+  return (
+    <div className="w-1/2 mx-auto text-center mt-8">
+      <Head>
+        <title>Página não encontrada.</title>
+      </Head>
+      <h1 className="font-bold text-4xl">URL não encontrada.</h1>
+      <p>Estamos redirecionando você para a central de links.</p>
+    </div>
+  );
 };
 
-export async function getServerSideProps( { params, res } ) {
-    const client = Prismic.client("https://fgarcia.cdn.prismic.io/api/v2");
-    const link = await client.getByUID("shorlink", params.slug);
-    if(link){
-        res.statusCode = 301 //conteudo movido permanentemente
-        res.setHeader("Location", link.data.destino.url) //redireciona
-        res.end();
-        return;
-    }
-    console.log(link)
-    return{
-        props:{}
-    };
+export async function getServerSideProps({ params, res }) {
+  const client = Prismic.client("https://fgarcia.cdn.prismic.io/api/v2");
+  const link = await client.getByUID("shortlink", params.slug);
+  if (link) {
+    res.statusCode = 301; // conteudo movido permanentemente
+    res.setHeader("Location", link.data.destino.url); // redireciona
+    res.end();
+    return;
+  }
+  return {
+    props: {},
+  };
 }
 
-    export default RedirectTo;
+export default RedirectTo;
